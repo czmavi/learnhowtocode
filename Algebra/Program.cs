3fbc3e347d24775c7c4b79f15cfe7df38fc84b24
Martin Vích <mavi.cz@hotmail.com>
2018-07-26 18:43:06 +0200

    Add project files.

---
+++ Algebra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algebra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
